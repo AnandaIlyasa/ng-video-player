c60c579da4a707bbeb60dd9c58760c8f530b89eb
AnandaIlyasa <anandailyasa23244@student.uns.ac.id>
2024-03-17 14:40:34 +0700

    first commit & done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import videojs from 'video.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'web-video-player';
+
+  player: any;
+  isPlaying = false;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.player = videojs('my-video');
+  }
+
+  play(): void {
+    this.player.play();
+    this.isPlaying = true;
+  }
+
+  restart(): void {
+    this.player.currentTime(0);
+  }
+
+  fastForward(): void {
+    const currentTime = this.player.currentTime();
+    this.player.currentTime(currentTime + 10);
+  }
+  fastBackward(): void {
+    const currentTime = this.player.currentTime();
+    this.player.currentTime(currentTime - 10);
+  }
+
+  pause(): void {
+    this.player.pause();
+    this.isPlaying = false;
+  }
 }
